02bb83a9d5a0a07a53e36ec0d26270621bd4a9f1
oplde <knepsis@hotmail.com>
2019-06-27 13:05:14 +0100

    fix db

--- lunchmap/rango/models.py
+++ lunchmap/rango/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+import math
+import sys
 class Place(models.Model):
-    address = models.CharField()
-    google_id = models.CharField()
-    information = models.CharField()
-    phone_number = models.CharField()
-    rating = models.DecimalField(decimal_places=1)
+    address = models.CharField(max_length=sys.maxsize)
+    google_id = models.CharField(max_length=sys.maxsize)
+    information = models.CharField(max_length=sys.maxsize)
+    phone_number = models.CharField(max_length=sys.maxsize)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
 
-class Review(models.Model):
+# class Review(models.Model):
 
