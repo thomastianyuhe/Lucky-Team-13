693c6c0b73137cc6fcc2a164ee8e8555f7aa4fe9
oplde <knepsis@hotmail.com>
2019-06-27 14:01:49 +0100

    add reviews

--- lunchmap/rango/admin.py
+++ lunchmap/rango/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rango.models import *
 
 # Register your models here.
+
+admin.site.register(Place)
+admin.site.register(Review)
--- lunchmap/rango/models.py
+++ lunchmap/rango/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-import math
 import sys
+from django.contrib.auth.models import User
 class Place(models.Model):
     address = models.CharField(max_length=sys.maxsize)
     google_id = models.CharField(max_length=sys.maxsize)
@@ -8,5 +8,9 @@ class Place(models.Model):
     phone_number = models.CharField(max_length=sys.maxsize)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
 
-# class Review(models.Model):
+class Review(models.Model):
+    place_id = models.CharField(max_length=sys.maxsize)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=3, decimal_places=1)
+    comment = models.CharField(max_length=sys.maxsize)
 
