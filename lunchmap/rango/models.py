53fa58c4d268a69dca64787feb1de31a30ad0755
Sera <seraashworth@gmail.com>
2019-06-27 11:31:39 +0100

    Add models
    Add new models to represent Places and ratings

--- lunchmap/rango/models.py
+++ lunchmap/rango/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    address = models.CharField()
+    google_id = models.CharField()
+    information = models.CharField()
+    phone_number = models.CharField()
+    rating = models.DecimalField(decimal_places=1)
+
+class Review(models.Model):
+
